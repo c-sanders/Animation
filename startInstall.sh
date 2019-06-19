d7570b601b723e8cb6d3ee8a49fdeb7a281aa727
Fart Ball <fartball@fartball.com>
2019-06-19 18:41:30 +1000

    Minor changes.

--- startInstall.sh
+++ startInstall.sh
@@ -1,9 +1,28 @@
-#! /usr/bin/python3
+#! /bin/bash
 
 
-import ConfigureScriptAgent
+CURRENT_DIR="$(pwd)"
+DIRNAME="$(dirname $0)"
+FILENAME="$(basename $0)"
+
+PYTHON_DIR="${CURRENT_DIR}/${DIRNAME}/python"
+PYTHON_FILE="${PYTHON_DIR}/runInstaller.py"
+
+echo "Current dir = $CURRENT_DIR"
+echo "Python dir  = $PYTHON_DIR"
+echo "Python file = $PYTHON_FILE"
+echo "Arg 0       = $0"
+echo "Filename    = ${FILENAME}"
+echo "Dirname     = ${DIRNAME}"
+
+# We don't necessarily want to export the PYTHONPATH environment variable which is set in the
+# command below. The reason for this is because the calling environment might already have it set
+# the way it wants.
+#
+# Therefore, use the BASH set command line utility to just set the PYTHONPATH it locally for this
+# command.
+
+set PYTHONPATH=${PYTHON_DIR} ; python3 ${PYTHON_FILE}
 
 
-configureScriptAgent = ConfigureScriptAgent.ConfigureScriptAgent()
-
-configureScriptAgent.run()
+exit 0
